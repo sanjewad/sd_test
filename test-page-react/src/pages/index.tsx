2b9ec1f3525ad14a0a3210c5761fd55a6136c987
Sanjeewa <sanjewad@yahoo.com>
2024-12-22 09:27:59 +1100

    add

--- test-page-react/src/pages/index.tsx
+++ test-page-react/src/pages/index.tsx
@@ -1,21 +1,5 @@
-import { useEffect, useState } from "react";
 
 export default function Home() {
-
-const[product, setProduct] =  useState('');
-
-useEffect(() => {
-  (async () => {
-    try{
-    const response = await fetch('/api/load/products')
-    const data = await response?.json()
-    setProduct(data)
-    console.log(product)
-    }catch(e){
-      console.log(">>>>"+ e)
-    }
-  })();
-}, []);
 
   return (
     <div>
