d078f0c7eb3cb5f2d31726c29a432de8f1cb9921
Sanjeewa <sanjewad@yahoo.com>
2024-12-22 08:09:44 +1100

    add

--- test-page-react/src/pages/index.tsx
+++ test-page-react/src/pages/index.tsx
@@ -20,6 +20,7 @@ useEffect(() => {
     <div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>
+            This is page from Sanj
           {product}
         </div>
       </main>
